89e58b35183044a4ea7b5a72e2f0a15c992cc4a6
lmunck <anders.munck@gmail.com>
2023-08-05 11:52:05 +0200

    dd

--- Sources/HighlightedTextEditor/HighlightedTextEditor.UIKit.swift
+++ Sources/HighlightedTextEditor/HighlightedTextEditor.UIKit.swift
@@ -43,6 +43,7 @@ public struct HighlightedTextEditor: UIViewRepresentable, HighlightingTextEditor
     public func makeUIView(context: Context) -> UITextView {
         let textView = UITextView()
         textView.delegate = context.coordinator
+        updateHeight(textView)
         updateTextViewModifiers(textView)
         
         return textView
